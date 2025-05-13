a075ff5a8f374596d031fe052881c21dc2ebbfb3
Vince <42940948+polymons@users.noreply.github.com>
2025-05-13 12:13:21 +0200

    fix head

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,8 @@ export const metadata: Metadata = {
 // Root layout must be a server component to support metadata exports
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">      <head>
+    <html lang="en">
+      <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="google" content="notranslate" />
