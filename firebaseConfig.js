d062e9d391a501bdd9134c40756f0fb1a08e0ae8
Vince <42940948+polymons@users.noreply.github.com>
2025-05-26 03:43:42 +0200

    config inline

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,12 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAPfS8eMu8E_r5BJnsu9M6aUERrBVvRJhk",
+  authDomain: "skrlinkredirect.firebaseapp.com",
+  databaseURL: "https://skrlinkredirect-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "skrlinkredirect",
+  storageBucket: "skrlinkredirect.appspot.com",
+  messagingSenderId: "554165848881",
+  appId: "1:554165848881:web:40d41f97c7c2d9c37abe10",
+  measurementId: "G-SFNMDXVN2M"
 };
